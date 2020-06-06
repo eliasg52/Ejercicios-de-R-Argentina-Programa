77d544c4fb32742567253e6f3ee6a0d9f7d3e4bd
eliasg52 <eliasgarcia81@gmail.com>
2020-06-06 18:55:09 -0300

    Tarea 3 Clase 5 Terminada

--- Clase5/tarea-clase-5-3.js
+++ Clase5/tarea-clase-5-3.js
@@ -5,11 +5,21 @@ const $segundosVideo = document.querySelectorAll('.segundos-video');
 const $resultadoTiempo = document.querySelector('#resultado-calcular-tiempo')
 
 $botonCalcularTiempo.onclick = function(){
+
 	let horasSumaTotal = 0;
 	for(let i = 0; i < $horasVideo.length; i++){
-		horasSumaTotal = horasSumaTotal + horasVideo[i].value;
+		horasSumaTotal = horasSumaTotal + Number($horasVideo[i].value);
 	}
 
-	// $horasVideo.innerText = $resultadoTiempo;
+	let minutosSumaTotal = 0;
+	for(let i = 0; i < $minutosVideo.length; i++){
+		minutosSumaTotal = minutosSumaTotal + Number($minutosVideo[i].value);
+	}
 
+	let segundosSumaTotal = 0;
+	for(let i = 0; i < $segundosVideo.length; i++){
+		segundosSumaTotal = segundosSumaTotal + Number($segundosVideo[i].value);
+	}
+	
+	$resultadoTiempo.innerText = `El Tiempo total de las clases es ${horasSumaTotal}hs ${minutosSumaTotal}min ${segundosSumaTotal}sec.` 
 }
