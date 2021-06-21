daf2fa7c39a6b97d8d9feba2281604782ef31292
Elias Garcia.- <50644355+eliasg52@users.noreply.github.com>
2021-06-21 01:15:40 -0300

    update

--- Clase10-Simon/script.js
+++ Clase10-Simon/script.js
@@ -1,70 +1,50 @@
 const colors = document.querySelectorAll('.colors');
 const startButton = document.querySelector('#start-button');
 const simon = document.querySelector('#simon');
+const cpuPlayer = document.querySelector('#cpu-turn');
+const humanPlayer = document.querySelector('#player-turn');
 
 startButton.addEventListener('click', () => {
   /*   const hiddeButton = startButton.classList.add('hidden'); */
-  cpuTurn(colors);
+
+  setTimeout(() => {
+    cpuTurn(colors);
+  }, 1000);
+
+  setTimeout(() => {
+    playerTurn();
+  }, 2000);
+
+  /*   setTimeout(() => {
+    checkColors(cpuTurn(colors), playerTurn());
+  }, 3000); */
 });
-
-simon.addEventListener('click', (e) => {
-  const playerColor = e.target.id;
-  return playerColor;
-});
-
-if (cpuColor === 'red') {
-  console.log('asd');
-}
-
-function start() {
-  const cpuColor = cpuTurn(colors);
-
-  /*   setTimeout(function () {
-    console.log(playerColor);
-  }, 2000); */
-
-  /* setTimeout(playerTurn()), 3000; */
-  /*   setTimeout(function () {
-    checkColors(cpuColor, playerTurn());
-  }),
-    4000; */
-  /*   checkColors(cpuColor, playerTurn()); */
-}
 
 function cpuTurn(colors) {
+  console.log('inicio');
   const chooseRandomColor = Math.floor(Math.random() * 4);
 
   const randomColor = colors[chooseRandomColor];
   randomColor.classList.remove('opacity');
+  cpuPlayer.classList.remove('opacity');
 
   setTimeout(function () {
     randomColor.classList.add('opacity');
-  }, 1000);
-
-  const cpuColor = randomColor.id;
-  return cpuColor;
+    cpuPlayer.classList.add('opacity');
+    console.log('fin');
+    const cpuColor = randomColor.id;
+    console.log(cpuColor);
+    return cpuColor;
+  }, 1500);
 }
-
-/* function playerTurn() {
-  simon.addEventListener('click', obtenerColor);
-} */
 
 function checkColors(cpuColor, playerColor) {
   if (cpuColor === playerColor) {
-    console.log('SI');
+    console.log('SIIIIIII');
   } else {
     console.log('NO');
   }
 }
-/* 
-function obtenerColor(eventoTarget) {
-  
-}
-
-function consolear(asd) {
-  const colorPlayer = asd;
-  return colorPlayer;
-} */
 
 function obtenerColor(e) {
   const color = e.target.id;
